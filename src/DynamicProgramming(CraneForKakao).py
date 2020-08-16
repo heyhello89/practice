a42716d9090e6f77c6ed4516ebe8534f2e4d2a45
heyhello89 <heyhello89@naver.com>
2020-08-16 15:07:54 +0900

    CraneForKakao

--- src/DynamicProgramming(CraneForKakao).py
+++ src/DynamicProgramming(CraneForKakao).py
@@ -30,4 +30,4 @@ def solution(board, moves):
 if __name__ == '__main__':
     board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
     moves = [1, 5, 3, 5, 1, 2, 1, 4]
-    solutin(board, moves)
+    solution(board, moves)
